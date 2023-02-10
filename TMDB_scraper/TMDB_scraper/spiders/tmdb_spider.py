349580a126aecb9a697eb138f6b648b0ebfb942d
haominmai <haominmai@gmail.com>
2023-02-09 23:57:18 -0800

    Web-Scraping

--- TMDB_scraper/TMDB_scraper/spiders/tmdb_spider.py
+++ TMDB_scraper/TMDB_scraper/spiders/tmdb_spider.py
@@ -16,10 +16,13 @@ class TmdbSpider(scrapy.Spider):
         # our goal is: https://www.themoviedb.org/movie/157336-interstellar/cast
 
         # get link for cast & crew page
+        
         cast_crew = response.css('p.new_button a::attr(href)').get()
-        #this gives us '/movie/157336-interstellar/cast'
+        
+        # this gives us '/movie/157336-interstellar/cast'
         
         cast_crew = response.urljoin(cast_crew)
+        
         # now we have 'https://www.themoviedb.org/movie/157336-interstellar/cast'
         
         yield scrapy.Request(cast_crew, callback=self.parse_full_credits)
@@ -31,17 +34,21 @@ class TmdbSpider(scrapy.Spider):
         '''
         
         # address to actor part
-        actor = response.css('ol.people.credits')
-        actor = actor.css('div.info')
+        
+        actor = response.css('.people.credits:not(.crew) div.info')
         
         # get the link to each actor 
+        
         actor_link = actor.css('a::attr(href)').getall()
+        
         # it gives something like '/person/10297-matthew-mcconaughey'
         
         for link in actor_link:
             
             # get the whole link
+            
             whole_link = response.urljoin(link)
+            
             # we would get something like 'https://www.themoviedb.org/person/10297-matthew-mcconaughey'
             
             yield scrapy.Request(whole_link, callback = self.parse_actor_page)
@@ -55,10 +62,13 @@ class TmdbSpider(scrapy.Spider):
         '''
         
         # extract actor's name from title 
+        
         actor_name = response.css('h2.title a::text').get()
         
         # get the list of all movies or TV shows on which that actor has worked
+        
         worked_list = response.css('div.credits_list a.tooltip bdi::text').getall()
         
         for movie_or_TV_name in worked_list:
+            
             yield {"actor" : actor_name, "movie_or_TV_name" : movie_or_TV_name}
